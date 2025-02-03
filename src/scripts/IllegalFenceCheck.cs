e3719019de0b9246f3d81f03060b84fa2229a5b7
Ace <aceshyjan@gmail.com>
2025-02-03 00:10:37 +0000

    feat more IFS algorithms

--- src/scripts/IllegalFenceCheck.cs
+++ src/scripts/IllegalFenceCheck.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 public partial class IllegalFenceCheck : Node
 {
 	private readonly int[] _bits = { 0, 1 };
+
 	public void GetIllegalFences(BoardState board)
 
 	{
@@ -35,7 +36,7 @@ public partial class IllegalFenceCheck : Node
 			});
 		});
 
-		Console.Call("add_entry", "Illegal fence check took: " + (DateTime.Now.Ticks - startTime) / 10000 + "ms", 0);
+		Console.Call("add_entry", "Illegal fence check took: " + (DateTime.Now.Ticks - startTime) / 100 + "ns", 0);
 	}
 
 	private bool IsFenceIllegal(BoardState board, int fence, int direction, int player)
@@ -48,7 +49,7 @@ public partial class IllegalFenceCheck : Node
 		int start = boardClone.GetPawnPosition(player);
 		HashSet<int> goalTiles = boardClone.GetGoalTiles(player).ToHashSet();
 
-		return IterativeDFS(boardClone, start, goalTiles);
+		return RecursiveBFS(boardClone, start, goalTiles, new HashSet<int>());
 	}
 
 	private bool IterativeDFS(BoardState board, int start, HashSet<int> goalTiles)
@@ -75,6 +76,114 @@ public partial class IllegalFenceCheck : Node
 				stack.Push(connectedTile);
 			}
 		}
+		return true;
+	}
+
+	public bool RecursiveDFS(BoardState board, int current, HashSet<int> goalTiles, HashSet<int> visited)
+	{
+		if (goalTiles.Contains(current)) return false;
+
+		if (visited.Contains(current)) return true;
+
+		visited.Add(current);
+
+		foreach (int connectedTile in board.GetTileConnections(current).Reverse())
+		{
+			if (visited.Contains(connectedTile) || connectedTile == -1) continue;
+
+			if (!RecursiveDFS(board, connectedTile, goalTiles, visited)) return false;
+		}
+
+		return true;
+	}
+
+	public bool IterativeBFS(BoardState board, int start, HashSet<int> goalTiles)
+	{
+		Queue<int> queue = new();
+		HashSet<int> visited = new();
+
+		queue.Enqueue(start);
+
+		while (queue.Count > 0)
+		{
+			int current = queue.Dequeue();
+
+			if (goalTiles.Contains(current)) return false;
+
+			if (visited.Contains(current)) continue;
+
+			visited.Add(current);
+
+			foreach (int connectedTile in board.GetTileConnections(current))
+			{
+				if (visited.Contains(connectedTile) || connectedTile == -1) continue;
+
+				queue.Enqueue(connectedTile);
+			}
+		}
+		return true;
+	}
+
+	public bool RecursiveBFS(BoardState board, int current, HashSet<int> goalTiles, HashSet<int> visited)
+	{
+		if (goalTiles.Contains(current)) return false;
+
+		if (visited.Contains(current)) return true;
+
+		visited.Add(current);
+
+		foreach (int connectedTile in board.GetTileConnections(current))
+		{
+			if (visited.Contains(connectedTile) || connectedTile == -1) continue;
+
+			if (!RecursiveBFS(board, connectedTile, goalTiles, visited)) return false;
+		}
+
+		return true;
+	}
+
+	public bool IterativeAStar(BoardState board, int start, HashSet<int> goalTiles, int player)
+	{
+		HashSet<int> visited = new();
+		PriorityQueue<int, int> queue = new();
+
+		queue.Enqueue(start, 0);
+
+		while (queue.Count > 0)
+		{
+			int current = queue.Dequeue();
+
+			if (goalTiles.Contains(current)) return false;
+
+			if (visited.Contains(current)) continue;
+
+			visited.Add(current);
+
+			foreach (int connectedTile in board.GetTileConnections(current))
+			{
+				if (visited.Contains(connectedTile) || connectedTile == -1) continue;
+
+				queue.Enqueue(connectedTile, board.GetDistanceToGoal(connectedTile, player));
+			}
+		}
+		return true;
+	}
+
+	public bool RecursiveAStar(BoardState board, int current, HashSet<int> goalTiles, HashSet<int> visited, int player)
+	{
+		if (goalTiles.Contains(current)) return false;
+
+		if (visited.Contains(current)) return true;
+
+		visited.Add(current);
+
+		foreach (int connectedTile in board.GetTileConnections(current))
+		{
+			if (visited.Contains(connectedTile) || connectedTile == -1) continue;
+
+			if (!RecursiveAStar(board, connectedTile, goalTiles, visited, player)) return false;
+		}
+
 		return true;
 	}
 }
