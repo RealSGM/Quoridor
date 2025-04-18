210620025efc1c087d07bfcaa69ceb0e140f4cce
Ace <aceshyjan@gmail.com>
2025-04-18 19:47:33 +0100

    cleanup

--- src/scripts/board/FenceBB.cs
+++ src/scripts/board/FenceBB.cs
@@ -1,9 +1,9 @@
-public class FenceBB(ulong fences = 0)
+public readonly struct FenceBB(ulong fences)
 {
-    public ulong Fences { get; private set; } = fences;
+    public ulong Fences { get; init; } = fences;
 
     public FenceBB Clone() => new(Fences);
     public bool IsPlaced(int index) => (Fences & (1UL << index)) != 0;
-    public void SetPlaced(int index) => Fences |= 1UL << index;
-    public void UndoSetPlaced(int index) => Fences &= ~(1UL << index);
+    public FenceBB SetPlaced(int index) => new(Fences | (1UL << index));
+    public FenceBB UndoSetPlaced(int index) => new(Fences & ~(1UL << index));
 }
--- src/scripts/board/Pawn.cs
+++ src/scripts/board/Pawn.cs
@@ -1,15 +1,9 @@
-public struct Pawn 
+public struct Pawn(byte index, int fencesRemaining = Helper.MaxFences)
 {
-    public int FencesRemaining { get; set; }
-    public byte Index { get; private set; }
+    public sbyte FencesRemaining { get; set; } = (sbyte) fencesRemaining;
+    public byte Index { get; private set; } = index;
 
-    public Pawn(byte index, int fencesRemaining = Helper.MaxFences)
-    {
-        Index = index;
-        FencesRemaining = fencesRemaining;
-    }
-
-    public Pawn Clone() => new(Index, FencesRemaining);
+    public readonly Pawn Clone() => new(Index, FencesRemaining);
     public void PlaceFence() => FencesRemaining--;
     public void UndoPlaceFence() => FencesRemaining++;
     public void Move(byte index) => Index = index;
